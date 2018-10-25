c60f26d1ca8ad4f0ae5b7241585c504b45c83fbb
mo-ma <mmahdavi79>
2018-10-25 15:15:14 +0330

    Player added

---
+++ src/Block.java
@@ -0,0 +1,9 @@
+public class Block {
+    public int id;
+    Block(int id){
+        this.id=id;
+    }
+    int remove(){
+        return 0;
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,32 @@
+import java.lang.Math;
+import java.util.ArrayList;
+
+public class Player{
+    public Army army=null;
+    public int gills =30000;
+    public ArrayList<Block> blocks=new ArrayList<>();
+    int getNextId(){
+        int mx=0;
+        for(Block block:blocks){
+            mx=Math.max(mx,block.id);
+        }
+        return mx+1;
+    }
+    void addBlock(){
+        int id=getNextId();
+        Block block =new Block(id);
+        blocks.add(block);
+        System.out.println(id);
+    }
+    void removeBlock(int id){
+        for(Block block:blocks){
+            if(block.id==id){
+                gills-=block.remove();
+                blocks.remove(block);
+                return;
+            }
+        }
+        System.out.println("not possible");
+    }
+
+}
